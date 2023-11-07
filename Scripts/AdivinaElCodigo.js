86e19269edf211ca3e25d2e97f4a0166514edad9
sjo <narcis.matas.7e7@itb.cat>
2023-11-07 13:43:27 +0100

    Final clase 7/11

--- Scripts/AdivinaElCodigo.js
+++ Scripts/AdivinaElCodigo.js
@@ -2,16 +2,28 @@ let targetNum = Math.floor(Math.random() * (99999 - 10000 +1) + 10000);
 console.log(targetNum);
 let separatedTarget = targetNum.toString().split('');
 console.log(separatedTarget)
+
 function onGuess(){
-    let guess = document.getElementById("numInputBox");
+    let guess = document.getElementById("numInputBox").value;
     console.log(guess.value);
-    let separatedGuess = guess.value.split('');
+    let separatedGuess = guess.split('');
     console.log(separatedGuess)
-    guess.value = '';
-    if(separatedGuess == separatedTarget){
+    
+    if(checkEqual(separatedGuess, separatedTarget)){
         win();
     }
     else{
         attempt();
     }
+    document.getElementById("numInputBox").value = '';
+}
+function checkEqual(array1, array2){
+    let equal = true
+
+    for(i = 0; i < array1.size(); i++){
+        if(array1[i] != array2[i]){
+            equal = false
+        }
+    }
+    return equal;
 }
