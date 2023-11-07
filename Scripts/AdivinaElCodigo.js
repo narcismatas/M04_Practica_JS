672a0b29917df45bd5fd78c02324e318d92021bb
sjo <narcis.matas.7e7@itb.cat>
2023-11-07 13:57:32 +0100

    A

--- Scripts/AdivinaElCodigo.js
+++ Scripts/AdivinaElCodigo.js
@@ -5,22 +5,44 @@ console.log(separatedTarget)
 
 function onGuess(){
     let guess = document.getElementById("numInputBox").value;
-    console.log(guess.value);
-    let separatedGuess = guess.split('');
-    console.log(separatedGuess)
-    
-    if(checkEqual(separatedGuess, separatedTarget)){
-        win();
+    console.log(guess);
+    document.getElementById("numInputBox").value = '';
+    if(checkValid(guess)){
+
+        let separatedGuess = guess.split('');
+        console.log(separatedGuess)
+
+        if(checkEqual(separatedGuess, separatedTarget)){
+            win();
+        }
+        else{
+            attempt();
+        }
     }
     else{
-        attempt();
+        document.getElementById("message").value = "Valor no valido"
     }
-    document.getElementById("numInputBox").value = '';
+    
+    
 }
+
+function checkValid(guess){
+    if(guess.length == 5){
+        for(i = 0; i < guess.split(''); i++){
+            if(!isInteger(guess[i])){
+                return false;
+            }
+        }
+    }
+    else{
+        return false;
+    }
+    return true;
+}
+
 function checkEqual(array1, array2){
     let equal = true
-
-    for(i = 0; i < array1.size(); i++){
+    for(i = 0; i < 5; i++){
         if(array1[i] != array2[i]){
             equal = false
         }
