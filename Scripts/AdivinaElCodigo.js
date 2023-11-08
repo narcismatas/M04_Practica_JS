9332d0e44765fc982c0117608aafbdaa0ee96fa3
Narcis <narcis.matas.7e7@itb.cat>
2023-11-08 19:05:06 +0100

    Fi practica

--- Scripts/AdivinaElCodigo.js
+++ Scripts/AdivinaElCodigo.js
@@ -1,63 +1,71 @@
 let targetNum = Math.floor(Math.random() * (99999 - 10000 +1) + 10000);
-console.log(targetNum);
+//console.log(targetNum);
 let separatedTarget = targetNum.toString().split('');
-console.log(separatedTarget);
 let guessNum = 0;
 let guessArea = document.getElementById("guesses");
 let won = false;
-let delay = 100;
+var reloadButton = false
+
+let digCountTarget = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+for (i = 0; i < 5; i++){
+    digCountTarget[parseInt(separatedTarget[i])]++
+}
 
 function onGuess(){
-    let guess = document.getElementById("numInputBox").value;
-    
-    document.getElementById("numInputBox").value = '';
-    if(checkValid(guess) && !won){
-        guessNum++;
-        let separatedGuess = guess.split('');
-        console.log(separatedGuess);
-        console.log(guess);
-        if(checkEqual(separatedGuess, separatedTarget)){
-            win(separatedGuess);
+
+    if(!reloadButton){
+        let guess = document.getElementById("numInputBox").value;
+        
+        document.getElementById("numInputBox").value = '';
+        if(checkValid(guess) && !won){
+            guessNum++;
+            let separatedGuess = guess.split('');
+            if(checkEqual(separatedGuess, separatedTarget)){
+                win(separatedGuess);
+            }
+            else{
+                if (guessNum <= 5){
+                    attempt(separatedGuess);
+                }
+            }
+            
         }
         else{
-            if (guessNum <= 5){
-                attempt(separatedGuess);
+            if(won){
+                document.getElementById("message").innerText = "Ya ganaste!"           
+            }
+            else if(guessNum < 5){
+                switch(guessNum){
+                    case 0:
+                        document.getElementById("message").innerText = "Valor inválido. Sigues en el primer intento.";
+                        break;
+                    case 1:
+                        document.getElementById("message").innerText = "Valor inválido. Tienes 4 intentos restantes";
+                        break;
+                    case 2:
+                        document.getElementById("message").innerText = "Valor inválido. Tienes 3 intentos restantes";
+                        break;
+                    case 3:
+                        document.getElementById("message").innerText = "Valor inválido. Tienes 2 intentos restantes";
+                        break;
+                    case 4:
+                        document.getElementById("message").innerText = "Valor inválido. Estas en el ultimo intento";
+                        break;
+                }
+                //document.getElementById("message").innerText = "Valor inválido. Sigues en el primer intento."
             }
         }
     }
     else{
-        if(won){
-            document.getElementById("message").innerText = "Ya ganaste!"           
-        }
-        else if(guessNum < 5){
-            switch(guessNum){
-                case 0:
-                    document.getElementById("message").innerText = "Valor inválido. Sigues en el primer intento.";
-                    break;
-                case 1:
-                    document.getElementById("message").innerText = "Valor inválido. Tienes 4 intentos restantes";
-                    break;
-                case 2:
-                    document.getElementById("message").innerText = "Valor inválido. Tienes 3 intentos restantes";
-                    break;
-                case 3:
-                    document.getElementById("message").innerText = "Valor inválido. Tienes 2 intentos restantes";
-                    break;
-                case 4:
-                    document.getElementById("message").innerText = "Valor inválido. Estas en el ultimo intento";
-                    break;
-            }
-            //document.getElementById("message").innerText = "Valor inválido. Sigues en el primer intento."
-        }
-        else{
-
-        }
+        location.reload()
     }
+    
 }
 
 
 
-function win(userGuess){
+async function win(userGuess){
+    document.getElementById("send").innerText = "NUEVA PARTIDA"
     document.getElementById("message").style.backgroundColor = "green";
     document.getElementById("message").innerText = "Felicidades! Has acertado."
     const guessedNum = document.createElement("div");
@@ -65,40 +73,64 @@ function win(userGuess){
     const guessRow = document.getElementById("guesses").appendChild(guessedNum);
     
     for (i = 0; i < 5; i++){
-        let digit = document.createElement("div")
-        digit.className = 'guessDigitGreen'
+        let digit = document.createElement("div");
+        digit.className = 'guessDigitGreen';
         digitText = document.createElement("p");
         digitText.innerText = userGuess[i];
         digit.appendChild(digitText);
         guessRow.appendChild(digit);
-        document.getElementsByName("secretdigit")[i].innerHTML = separatedTarget[i]
+        digit.style.animation = "fade-in 0.3s";
+        document.getElementsByName("secretdigit")[i].innerHTML = separatedTarget[i];
+
+        await delay(300)
     }
-
-    
-
-    won = true
+    reloadButton = true;
+    won = true;
+    document.getElementById("send").innerText = "NUEVA PARTIDA";
 }
 
-function attempt(userGuess){
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function attempt(userGuess){
 
     const guessedNum = document.createElement("div");
     guessedNum.className = 'numDisplay';
     const guessRow = document.getElementById("guesses").appendChild(guessedNum);
+    let availableDig = Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    for(i = 0; i < 10; i++){
+        availableDig[i] = digCountTarget[i]
+    }
+     
+    
+    
+    let guessColors = Array("Gray", "Gray", "Gray", "Gray", "Gray")
+    for (i = 0; i < 5; i++){
+        if(userGuess[i] == separatedTarget[i]){
+            guessColors[i] = "Green";
+            availableDig[parseInt(userGuess[i])]--;
+        }
+    }
 
     for (i = 0; i < 5; i++){
+
         
-        let state = "Gray";
-        for (j = 0; j < 5; j++){
-            if(userGuess[i] == separatedTarget[j] && i == j){
-                state = "Green";
-            }
-            else if(state != "Green" && userGuess[i] == separatedTarget[j]){
-                state = "Yellow";
+
+        if(guessColors [i] != "Green" && availableDig[parseInt(userGuess[i])] > 0){
+            let found = false
+            for (j = 0; j < 5; j++){
+                if(userGuess[i] == separatedTarget[j] && !found){
+                    guessColors[i] = "Yellow";
+                    availableDig[parseInt(userGuess[i])]--;
+                    found = true;
+                }
             }
         }
+        
 
         let digit = document.createElement("div")
-        switch(state){
+        switch(guessColors[i]){
             case "Gray":
                 digit.className = 'guessDigitGray';
                 break;
@@ -109,12 +141,13 @@ function attempt(userGuess){
                 digit.className = 'guessDigitGreen';
                 break;
         }
-        
         digitText = document.createElement("p");
         digitText.innerText = userGuess[i];
         digit.appendChild(digitText);
         guessRow.appendChild(digit);
+        digit.style.animation = "fade-in 0.3s"
         
+        await delay(200);
     }
     switch(guessNum){
         case 1:
@@ -133,15 +166,16 @@ function attempt(userGuess){
             document.getElementById("message").innerText = "Vaya, no lo acertaste";
             fail()
             break;
-
     }
-    
 }
 
 function fail(){
+    document.getElementById("send").innerText = "NUEVA PARTIDA"
+    
     for(i = 0; i < 5; i++){
         document.getElementsByName("secretdigit")[i].innerHTML = separatedTarget[i]
     }
+    
 }
 
 function checkValid(guess){
