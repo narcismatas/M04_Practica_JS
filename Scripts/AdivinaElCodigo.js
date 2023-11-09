cebdfa5a254f9bbacace0b5f8c42ce99b97c56ab
Narcis <narcis.matas.7e7@itb.cat>
2023-11-09 15:50:29 +0100

    Extras

--- Scripts/AdivinaElCodigo.js
+++ Scripts/AdivinaElCodigo.js
@@ -1,4 +1,6 @@
-let targetNum = Math.floor(Math.random() * (99999 - 10000 +1) + 10000);
+
+
+let targetNum = Math.floor(Math.random() * (99999 - 10000 + 1) + 10000);
 //console.log(targetNum);
 let separatedTarget = targetNum.toString().split('');
 let guessNum = 0;
@@ -7,36 +9,36 @@ let won = false;
 var reloadButton = false
 
 let digCountTarget = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-for (i = 0; i < 5; i++){
+for (i = 0; i < 5; i++) {
     digCountTarget[parseInt(separatedTarget[i])]++
 }
 
-function onGuess(){
+function onGuess() {
 
-    if(!reloadButton){
+    if (!reloadButton) {
         let guess = document.getElementById("numInputBox").value;
-        
+
         document.getElementById("numInputBox").value = '';
-        if(checkValid(guess) && !won){
+        if (checkValid(guess) && !won) {
             guessNum++;
             let separatedGuess = guess.split('');
-            if(checkEqual(separatedGuess, separatedTarget)){
+            if (checkEqual(separatedGuess, separatedTarget)) {
                 document.getElementById("numInputBox").disabled = true;
                 win(separatedGuess);
             }
-            else{
-                if (guessNum <= 5){
+            else {
+                if (guessNum <= 5) {
                     attempt(separatedGuess);
                 }
             }
-            
+
         }
-        else{
-            if(won){
-                document.getElementById("message").innerText = "Ya ganaste!"           
+        else {
+            if (won) {
+                document.getElementById("message").innerText = "Ya ganaste!"
             }
-            else if(guessNum < 5){
-                switch(guessNum){
+            else if (guessNum < 5) {
+                switch (guessNum) {
                     case 0:
                         document.getElementById("message").innerText = "Valor inválido. Sigues en el primer intento.";
                         break;
@@ -57,24 +59,24 @@ function onGuess(){
             }
         }
     }
-    else{
+    else {
         location.reload()
     }
-    
+
 }
 
 
 
-async function win(userGuess){
+async function win(userGuess) {
     document.getElementById("send").innerText = "NUEVA PARTIDA"
     document.getElementById("message").style.backgroundColor = "green";
     document.getElementById("message").innerText = "Felicidades! Has acertado."
     const guessedNum = document.createElement("div");
     guessedNum.className = 'numDisplay';
     const guessRow = document.getElementById("guesses").appendChild(guessedNum);
-    
-    
-    for (i = 0; i < 5; i++){
+
+
+    for (i = 0; i < 5; i++) {
         let digit = document.createElement("div");
         digit.className = 'guessDigitGreen';
         digitText = document.createElement("p");
@@ -89,50 +91,268 @@ async function win(userGuess){
     reloadButton = true;
     won = true;
     document.getElementById("send").innerText = "NUEVA PARTIDA";
+
+    //CODI EXTERN CONFETTI
+
+    // Globals
+    var random = Math.random
+        , cos = Math.cos
+        , sin = Math.sin
+        , PI = Math.PI
+        , PI2 = PI * 2
+        , timer = undefined
+        , frame = undefined
+        , confetti = [];
+
+    var particles = 10
+        , spread = 40
+        , sizeMin = 3
+        , sizeMax = 12 - sizeMin
+        , eccentricity = 10
+        , deviation = 100
+        , dxThetaMin = -.1
+        , dxThetaMax = -dxThetaMin - dxThetaMin
+        , dyMin = .13
+        , dyMax = .18
+        , dThetaMin = .4
+        , dThetaMax = .7 - dThetaMin;
+
+    var colorThemes = [
+        function () {
+            return color(200 * random() | 0, 200 * random() | 0, 200 * random() | 0);
+        }, function () {
+            var black = 200 * random() | 0; return color(200, black, black);
+        }, function () {
+            var black = 200 * random() | 0; return color(black, 200, black);
+        }, function () {
+            var black = 200 * random() | 0; return color(black, black, 200);
+        }, function () {
+            return color(200, 100, 200 * random() | 0);
+        }, function () {
+            return color(200 * random() | 0, 200, 200);
+        }, function () {
+            var black = 256 * random() | 0; return color(black, black, black);
+        }, function () {
+            return colorThemes[random() < .5 ? 1 : 2]();
+        }, function () {
+            return colorThemes[random() < .5 ? 3 : 5]();
+        }, function () {
+            return colorThemes[random() < .5 ? 2 : 4]();
+        }
+    ];
+    function color(r, g, b) {
+        return 'rgb(' + r + ',' + g + ',' + b + ')';
+    }
+
+    // Cosine interpolation
+    function interpolation(a, b, t) {
+        return (1 - cos(PI * t)) / 2 * (b - a) + a;
+    }
+
+    // Create a 1D Maximal Poisson Disc over [0, 1]
+    var radius = 1 / eccentricity, radius2 = radius + radius;
+    function createPoisson() {
+        // domain is the set of points which are still available to pick from
+        // D = union{ [d_i, d_i+1] | i is even }
+        var domain = [radius, 1 - radius], measure = 1 - radius2, spline = [0, 1];
+        while (measure) {
+            var dart = measure * random(), i, l, interval, a, b, c, d;
+
+            // Find where dart lies
+            for (i = 0, l = domain.length, measure = 0; i < l; i += 2) {
+                a = domain[i], b = domain[i + 1], interval = b - a;
+                if (dart < measure + interval) {
+                    spline.push(dart += a - measure);
+                    break;
+                }
+                measure += interval;
+            }
+            c = dart - radius, d = dart + radius;
+
+            // Update the domain
+            for (i = domain.length - 1; i > 0; i -= 2) {
+                l = i - 1, a = domain[l], b = domain[i];
+                // c---d          c---d  Do nothing
+                //   c-----d  c-----d    Move interior
+                //   c--------------d    Delete interval
+                //         c--d          Split interval
+                //       a------b
+                if (a >= c && a < d)
+                    if (b > d) domain[l] = d; // Move interior (Left case)
+                    else domain.splice(l, 2); // Delete interval
+                else if (a < c && b > c)
+                    if (b <= d) domain[i] = c; // Move interior (Right case)
+                    else domain.splice(i, 0, c, d); // Split interval
+            }
+
+            // Re-measure the domain
+            for (i = 0, l = domain.length, measure = 0; i < l; i += 2)
+                measure += domain[i + 1] - domain[i];
+        }
+
+        return spline.sort();
+    }
+
+    // Create the overarching container
+    var container = document.createElement('div');
+    container.style.position = 'fixed';
+    container.style.top = '0';
+    container.style.left = '0';
+    container.style.width = '100%';
+    container.style.height = '0';
+    container.style.overflow = 'visible';
+    container.style.zIndex = '9999';
+
+    // Confetto constructor
+    function Confetto(theme) {
+        this.frame = 0;
+        this.outer = document.createElement('div');
+        this.inner = document.createElement('div');
+        this.outer.appendChild(this.inner);
+
+        var outerStyle = this.outer.style, innerStyle = this.inner.style;
+        outerStyle.position = 'absolute';
+        outerStyle.width = (sizeMin + sizeMax * random()) + 'px';
+        outerStyle.height = (sizeMin + sizeMax * random()) + 'px';
+        innerStyle.width = '100%';
+        innerStyle.height = '100%';
+        innerStyle.backgroundColor = theme();
+
+        outerStyle.perspective = '50px';
+        outerStyle.transform = 'rotate(' + (360 * random()) + 'deg)';
+        this.axis = 'rotate3D(' +
+            cos(360 * random()) + ',' +
+            cos(360 * random()) + ',0,';
+        this.theta = 360 * random();
+        this.dTheta = dThetaMin + dThetaMax * random();
+        innerStyle.transform = this.axis + this.theta + 'deg)';
+
+        this.x = window.innerWidth * random();
+        this.y = -deviation;
+        this.dx = sin(dxThetaMin + dxThetaMax * random());
+        this.dy = dyMin + dyMax * random();
+        outerStyle.left = this.x + 'px';
+        outerStyle.top = this.y + 'px';
+
+        // Create the periodic spline
+        this.splineX = createPoisson();
+        this.splineY = [];
+        for (var i = 1, l = this.splineX.length - 1; i < l; ++i)
+            this.splineY[i] = deviation * random();
+        this.splineY[0] = this.splineY[l] = deviation * random();
+
+        this.update = function (height, delta) {
+            this.frame += delta;
+            this.x += this.dx * delta;
+            this.y += this.dy * delta;
+            this.theta += this.dTheta * delta;
+
+            // Compute spline and convert to polar
+            var phi = this.frame % 7777 / 7777, i = 0, j = 1;
+            while (phi >= this.splineX[j]) i = j++;
+            var rho = interpolation(
+                this.splineY[i],
+                this.splineY[j],
+                (phi - this.splineX[i]) / (this.splineX[j] - this.splineX[i])
+            );
+            phi *= PI2;
+
+            outerStyle.left = this.x + rho * cos(phi) + 'px';
+            outerStyle.top = this.y + rho * sin(phi) + 'px';
+            innerStyle.transform = this.axis + this.theta + 'deg)';
+            return this.y > height + deviation;
+        };
+    }
+
+    function poof() {
+        if (!frame) {
+            // Append the container
+            document.body.appendChild(container);
+
+            // Add confetti
+            var theme = colorThemes[0]
+                , count = 0;
+            (function addConfetto() {
+                var confetto = new Confetto(theme);
+                confetti.push(confetto);
+                container.appendChild(confetto.outer);
+                timer = setTimeout(addConfetto, spread * random());
+            })(0);
+
+            // Start the loop
+            var prev = undefined;
+            requestAnimationFrame(function loop(timestamp) {
+                var delta = prev ? timestamp - prev : 0;
+                prev = timestamp;
+                var height = window.innerHeight;
+
+                for (var i = confetti.length - 1; i >= 0; --i) {
+                    if (confetti[i].update(height, delta)) {
+                        container.removeChild(confetti[i].outer);
+                        confetti.splice(i, 1);
+                    }
+                }
+
+                if (timer || confetti.length)
+                    return frame = requestAnimationFrame(loop);
+
+                // Cleanup
+                document.body.removeChild(container);
+                frame = undefined;
+            });
+        }
+    }
+
+    
+
+    poof();
+
+    await delay(2000)
+    //FINAL CODI EXTERN CONFETTI
 }
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function attempt(userGuess){
+async function attempt(userGuess) {
 
     const guessedNum = document.createElement("div");
     guessedNum.className = 'numDisplay';
     const guessRow = document.getElementById("guesses").appendChild(guessedNum);
     let availableDig = Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-    for(i = 0; i < 10; i++){
+    for (i = 0; i < 10; i++) {
         availableDig[i] = digCountTarget[i]
     }
-     
-    
-    
+
+
+
     let guessColors = Array("Gray", "Gray", "Gray", "Gray", "Gray")
-    for (i = 0; i < 5; i++){
-        if(userGuess[i] == separatedTarget[i]){
+    for (i = 0; i < 5; i++) {
+        if (userGuess[i] == separatedTarget[i]) {
             guessColors[i] = "Green";
             availableDig[parseInt(userGuess[i])]--;
         }
     }
     document.getElementById("numInputBox").disabled = true;
-    for (i = 0; i < 5; i++){
+    for (i = 0; i < 5; i++) {
 
-        
 
-        if(guessColors [i] != "Green" && availableDig[parseInt(userGuess[i])] > 0){
+
+        if (guessColors[i] != "Green" && availableDig[parseInt(userGuess[i])] > 0) {
             let found = false
-            for (j = 0; j < 5; j++){
-                if(userGuess[i] == separatedTarget[j] && !found){
+            for (j = 0; j < 5; j++) {
+                if (userGuess[i] == separatedTarget[j] && !found) {
                     guessColors[i] = "Yellow";
                     availableDig[parseInt(userGuess[i])]--;
                     found = true;
                 }
             }
         }
-        
+
 
         let digit = document.createElement("div")
-        switch(guessColors[i]){
+        switch (guessColors[i]) {
             case "Gray":
                 digit.className = 'guessDigitGray';
                 break;
@@ -148,13 +368,13 @@ async function attempt(userGuess){
         digit.appendChild(digitText);
         guessRow.appendChild(digit);
         digit.style.animation = "fade-in 0.3s"
-        
+
         await delay(200);
     }
     document.getElementById("numInputBox").disabled = false;
     document.getElementById("numInputBox").select();
 
-    switch(guessNum){
+    switch (guessNum) {
         case 1:
             document.getElementById("message").innerText = "4 intentos restantes";
             break;
@@ -175,38 +395,39 @@ async function attempt(userGuess){
     }
 }
 
-function fail(){
+function fail() {
     document.getElementById("send").innerText = "NUEVA PARTIDA"
     reloadButton = true;
-    for(i = 0; i < 5; i++){
+    for (i = 0; i < 5; i++) {
         document.getElementsByName("secretdigit")[i].innerHTML = separatedTarget[i]
         document.getElementsByName("secretdigit")[i].classList.add("discovered")
     }
-    
+
 }
 
-function checkValid(guess){
-    if(guess.length == 5){
+function checkValid(guess) {
+    if (guess.length == 5) {
         let splitguess = guess.split('')
-        for(i = 0; i < 5; i++){
-            if(!Number.isInteger(parseInt(splitguess[i]))){
+        for (i = 0; i < 5; i++) {
+            if (!Number.isInteger(parseInt(splitguess[i]))) {
                 return false;
             }
         }
         attempt
         return true;
     }
-    else{
+    else {
         return false;
     }
 }
 
-function checkEqual(array1, array2){
+function checkEqual(array1, array2) {
     let equal = true
-    for(i = 0; i < 5; i++){
-        if(array1[i] != array2[i]){
+    for (i = 0; i < 5; i++) {
+        if (array1[i] != array2[i]) {
             equal = false
         }
     }
     return equal;
 }
+
